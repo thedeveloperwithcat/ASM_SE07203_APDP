a594e0c74adf1ce9f3bc0a9de30b298915d27969
Thang BH01685 <thedeveloperwithcat@gmail.com>
2025-12-09 10:15:24 +0700

    Thang upcode

--- SE07203-F1/Controllers/RegisterController.cs
+++ SE07203-F1/Controllers/RegisterController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SE07203_F1.Data;
 using SE07203_F1.Models;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace SE07203_F1.Controllers
 {
@@ -17,6 +20,14 @@ namespace SE07203_F1.Controllers
             ViewBag.IsError = false;
             return View();
         }
+        private string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return Convert.ToBase64String(bytes);
+            }
+        }
 
         [HttpPost]
         public async Task<IActionResult> RegisterAccount(string Username, string Fullname, string Password)
@@ -24,10 +35,21 @@ namespace SE07203_F1.Controllers
             ViewBag.IsError = false;
             try
             {
+                // THÊM: Kiểm tra tên đăng nhập đã tồn tại chưa
+                var existingAccount = await _context.Accounts.FirstOrDefaultAsync(u => u.Username == Username);
+                if (existingAccount != null)
+                {
+                    ViewBag.IsError = true;
+                    ViewBag.Error = "Tên đăng nhập đã tồn tại. Vui lòng chọn tên khác.";
+                    return View("Index");
+                }
+
                 Account account = new Account();
                 account.Username = Username;
                 account.Fullname = Fullname;
-                account.Password = Password;
+                // Băm mật khẩu trước khi lưu
+                account.Password = HashPassword(Password);
+
                 _context.Accounts.Add(account);
                 await _context.SaveChangesAsync();
                 ViewBag.Success = true;
@@ -37,7 +59,7 @@ namespace SE07203_F1.Controllers
             catch (Exception ex)
             {
                 ViewBag.IsError = true;
-                ViewBag.Error = ex.Message;
+                ViewBag.Error = "Đã xảy ra lỗi trong quá trình đăng ký. Vui lòng thử lại.";
                 return View("Index");
             }
         }
