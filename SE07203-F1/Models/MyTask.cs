3b411ce417c089b508dd485d76821f1ef0b561be
Thang BH01685 <thedeveloperwithcat@gmail.com>
2025-12-09 22:04:17 +0700

    up2204

--- SE07203-F1/Models/MyTask.cs
+++ SE07203-F1/Models/MyTask.cs
@@ -13,9 +13,12 @@ namespace SE07203_F1.Models
         [DataType(DataType.Text)]
         public string Description { get; set; }
         [Required]
-        public int AccountId { get; set; }
+        public int? AccountId { get; set; }
         [Required]
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
+        public string Status { get; set; } = "new"; // new / in_progress / completed / overdue
+        public string Priority { get; set; } = "medium"; // low / medium / high
+        public DateTime? DueDate { get; set; }
         public Account? Account { get; set; }
         // chấm hỏi vì không phải dữ liệu lúc nào cũng phải có 
         public Category? Category { get; set; }
