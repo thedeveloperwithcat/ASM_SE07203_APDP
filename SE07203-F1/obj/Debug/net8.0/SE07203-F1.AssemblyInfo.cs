4dbc1a26ab061668d6caabe01e384fb72f7e007f
Dang <dang0945392289@gmail.com>
2025-12-10 13:43:52 +0700

    update

--- SE07203-F1/obj/Debug/net8.0/SE07203-F1.AssemblyInfo.cs
+++ SE07203-F1/obj/Debug/net8.0/SE07203-F1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SE07203-F1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af2631de27a38c4243376ccad88dcc0b3754d93b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd5519d08d3fcdbe10e111595bebec3deba9f74")]
 [assembly: System.Reflection.AssemblyProductAttribute("SE07203-F1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SE07203-F1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
